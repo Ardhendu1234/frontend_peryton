d1e3edf4e400117409a77954ac1780b4957e49f0
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-12 01:49:38 +0530

    card is updated

--- src/pages/ecommerce.js
+++ src/pages/ecommerce.js
@@ -70,16 +70,53 @@ function Ecommerce() {
         ))} 
     </div>
 
-    <div className='bg-pink-300 w-[85vw]'>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 bg-zinc-700 w-[85vw] p-[1vw]">
         {allProducts.filter((product)=>product.productType===type).map((item,key)=>(
-            <div>
-                {item.name}
-            </div>
+             <div key={key} class="container page-wrapper">
+             <div class="page-inner">
+               <div class="row">
+                 <div class="el-wrapper">
+                   <div class="box-up">
+                     <img class="img" src={item.imageUrls} alt=""/>
+                     <div class="img-info">
+                       <div class="info-inner">
+                         <span class="p-name">{item.name}</span>
+                         {/* <span class="p-company">Yeezy</span> */}
+                       </div>
+                       <div class="a-size">Stock : <span class="size">{item.stock}</span></div>
+                     </div>
+                   </div>
+           
+                   <div class="box-down">
+                     <div class="h-bg">
+                       <div class="h-bg-inner"></div>
+                     </div>
+           
+                     <a 
+                    onClick={() => handleButtonClick(item.name)}
+                     class="cart" href="#">
+                       <span className="price ">${item.price}</span>
+                       <span class="add-to-cart">
+                         <span class="txt">Buy Now</span>
+                       </span>
+                     </a>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
         ))}
     </div>
+    {selectedItem && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <ContactForm item={selectedItem} onClose={handleCloseForm} />
+        </div>
+      )}
      
     </div>
   );
 }
 
 export default Ecommerce;
+
+
