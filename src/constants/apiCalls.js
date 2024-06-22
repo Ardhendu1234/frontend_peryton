0e540bfc5ba79e185ddc2fbe4854104c320de7be
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-22 18:27:55 +0530

    some chnges

--- src/constants/apiCalls.js
+++ src/constants/apiCalls.js
@@ -2,4 +2,3 @@
 export const ECOMM_URL="https://103.184.192.5:9000/ecommerce"
 export const Category_url="https://103.184.192.5:9000/category"
 export const service_url="https://103.184.192.5:9000/service"
-
