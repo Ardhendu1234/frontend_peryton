a5d6bb448f7b034c40871cf6fb71a6b6af68392c
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-27 09:59:29 +0530

    some chnges

--- src/pages/RefundPolicy.jsx
+++ src/pages/RefundPolicy.jsx
@@ -59,6 +59,98 @@ const RefundPolicy = () => {
       <h2 className="text-4xl font-semibold text-white mb-4">Need help?</h2>
 
       <p className="mb-4">Contact us at <strong>info@www.peryton.in</strong> for questions related to refunds and returns.</p>
+
+      <h2 className="text-4xl font-semibold text-white mb-4">Refund/Return Policy</h2>
+      <p className="mb-4">
+        Peryton offers a (2) day refund policy on all new unopened equipment, starting from the date the product(s) was delivered to the customer. Anything beyond (2) days of delivery will not be eligible for return. If the product(s) received have been opened, dismantled, used, or damaged, no return options will be offered. A refund excluding a 2% restocking fee, payment gateway charges (if there are any), and shipping charges will be provided once Peryton confirms the returned equipment. Returned products should have the original packaging and accessories. Peryton reserves the right to refuse a return if any accessories are missing in the box.
+      </p>
+      <p className="mb-4">
+        Peryton offers replacement or return of any product if it is:
+        <ul className="list-disc pl-8 mb-6">
+          <li>Defective or dead on arrival (DOA)</li>
+          <li>Faulty or broken upon receiving</li>
+          <li>A different product than what was ordered</li>
+        </ul>
+        Peryton will return/replace products only if you inform them about the issue over mail or telephone within (48) hours after the product's arrival. Anything beyond (48) hours will not be eligible for return/replacement if the product is found to be in any of the above conditions.
+      </p>
+      <p className="mb-4">
+        While every attempt is made to have accurate information on our website and we strive to deliver quality products, sometimes mistakes can happen from our part. If there are any missing products, kindly take a picture before opening the seal of the ZIP Lock cover or the sealed Package to ensure your claim. We will not consider any missing claims if the seal is broken. If we have accepted any missing products due to our mistake in packaging or handling the order, we will either resend the product, if it is in stock, or provide a refund for the missing product.
+      </p>
+      <p className="mb-4">
+        <strong>Note:</strong>
+        <ol className="list-decimal pl-8 mb-6">
+          <li>Shipping charges for returns would be borne by the customer and are non-refundable.</li>
+          <li>For the defective or faulty product above conditions are applicable only if manufacturer approved.</li>
+        </ol>
+      </p>
+      <h3 className="text-3xl font-semibold text-white mb-4">Returns</h3>
+      <p className="mb-4">
+        <strong>Return Process</strong>
+        <ol className="list-decimal pl-8 mb-6">
+          <li>Request a Return/Exchange online or over the phone with a Peryton representative.</li>
+          <li>Once accepted, we will send you a return label if the returned product is defective. If the product is new and unwanted, you will need to arrange for return shipping to us. Tracking is mandatory for returns.</li>
+          <li>Please allow 3-5 business days for processing once your order arrives at our facility. Items requiring extensive testing could take up to 7-14 business days.</li>
+          <li>An exchange or credit will be issued to the customer once approved by a Peryton representative.</li>
+        </ol>
+        All returns and exchanges are at the discretion of Peryton.
+      </p>
+      <p className="mb-4">
+        We take pride in offering our customers the option to pay Cash-on-Delivery (COD). However, due to a significant number of customers abusing this service by denying packages upon delivery, we incur losses in shipping and revenue. Therefore, we are compelled to flag and cancel any COD order that appears suspicious or if the customer has a history of rejecting packages. In such cases, please contact our customer care to confirm your order and we will try to assist you.
+      </p>
+      
+      <h3 className="text-3xl font-semibold text-white mb-4">Warranty/Liability</h3>
+      <p className="mb-4">
+        Peryton provides no warranty of any kind for any of the equipment it sells or otherwise distributes. You assume all risks for any products purchased or received from Peryton. Peryton and its employees are not liable for any instance of injury to you or others, or damage or destruction of property. Additionally, Peryton is not liable for any damages, consequential or inconsequential, resulting from the use or application of any Peryton-provided products. We sell our equipment expressly without warranty or performance for any use or situation. It is up to the user to ensure that the products safely meet the user’s intended purpose.
+      </p>
+      
+      <h3 className="text-3xl font-semibold text-white mb-4">Shipping Terms and Conditions</h3>
+      <p className="mb-4">
+        <strong>Commercial Carriers</strong><br />
+        You accept Delhivery, DTDC, FedEx, Bluedart, DHL, Speedpost, and all other commercial carriers Peryton offers or selects to fulfill your order (“Mail Carrier(s)”) as reliable mail carriers and Peryton's carriers of choice.
+      </p>
+      <p className="mb-4">
+        <strong>Shipping Address</strong><br />
+        A complete and valid street address is required to ship your products. Orders cannot be shipped to the following:
+        <ul className="list-disc pl-8 mb-6">
+          <li>Hotel and Convention Center Addresses: Due to issues with packages getting lost at hotels and convention centers, a permanent, verified, residential, or established business address is required.</li>
+          <li>PO Boxes, including APO addresses: Shipments to PO Boxes and APO addresses are reviewed on a case-by-case basis and may be declined. To avoid your order being held for review, use your regular residential or business address.</li>
+        </ul>
+      </p>
+      <p className="mb-4">
+        <strong>Tracking and Lost Shipments</strong><br />
+        If you select “Free Shipping,” Peryton will send your packages with Delhivery (mostly surface). Depending on the type of Delhivery service chosen by Peryton, the package may show receipt by Delhivery but may not show any tracking information after it was received. Even if Peryton ships the items promptly to the Delhivery pick-up center, it might not be updated in the tracking immediately and will not be considered a liability of our same-day shipping policy. Peryton has no control over the internal processes of Mail Carriers or the local postal system and cannot be held liable for any mistakes made by a Mail Carrier.
+        If a package has been shipped under the free shipping offer and is lost in transit, Peryton's liability will be limited to Rs. 5,000/-.
+      </p>
+      <p className="mb-4">
+        <strong>Shipping Insurance</strong><br />
+        When Peryton sends your order with any Mail Carrier, the delivery is not insured. Optional shipping insurance may be offered for an additional charge at Peryton’s sole discretion.
+      </p>
+      <p className="mb-4">
+        <strong>Signature Requirements</strong><br />
+        Most mail services offered by Peryton require a signature upon delivery. Only in certain cases and at Peryton’s sole discretion will a signature not be required.
+      </p>
+      <p className="mb-4">
+        <strong>Backordered Items</strong><br />
+        If your order contains items that are back-ordered, your entire order will be held until the back-ordered item(s) are back in stock. All orders ship complete. Partial shipments are only available under certain conditions and at Peryton’s sole discretion. Items in your order that are not back-ordered will be held in reserve for you so that no previously purchased item runs out of stock while waiting for the back-ordered item(s).
+      </p>
+      <p className="mb-4">
+        <strong>Canceled Order Refunds</strong><br />
+        Orders declined by us due to a) violation of any of our shipping rules or b) suspected fraud, are automatically voided and canceled with your method of payment. In most cases, you will be notified of any order cancellations.
+      </p>
+      <p className="mb-4">
+        <strong>Shipment Times and Delays</strong><br />
+        Orders with all items in stock and placed before 3 PM IST Monday-Saturday typically ship the next day. Exceptions may occur due to holidays, payment review delays, or other unforeseen issues. Orders placed after 3 PM IST will typically not be processed until the next business day. Transit times for “Next-Day” and other expedited shipping options are calculated from the time the Mail Carrier picks up an order from our warehouse after processing. Sometimes there is a delay in pickup (2-3 days). In rare cases and depending on your ship-to address, it can take longer than indicated for a package to reach you. This delay is independent of the mail service you chose during checkout. The delivery time frames provided are averages for most mail pieces. Peryton is not responsible if a Mail Carrier delays the arrival of your package due to circumstances beyond our control. Peryton does not provide refunds if your package is delayed.
+      </p>
+      <p className="mb-4">
+        <strong>Free Shipping</strong><br />
+        Peryton offers Free Shipping on a majority of orders. Qualifying orders over Rs. 10,000 will be eligible for free Delhivery Surface shipping except when the weight (volumetric or gross) exceeds 2KG.
+        Some exceptions apply, such as specialty items noted on individual product pages. Batteries can only be shipped via the surface shipping method. While free shipping is still available for orders containing batteries, 2-3 day shipping cannot be guaranteed due to the required ground shipping method.
+        Specific items within the store are excluded from free shipping due to their large size and/or weight. Items that are excluded from free shipping are clearly marked at the bottom of each product page description. If an order contains one of these items, the entire order will not be eligible for free shipping.
+      </p>
+      <p className="mb-4">
+        <strong>Limits Of Responsibility</strong><br />
+        We accept no responsibility for improper installation of our products. Electrical polarity must be properly observed in hooking up electrical components.
+      </p>
     </div>
   );
 };
