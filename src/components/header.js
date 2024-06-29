c76b018373b0296e3fa581afe42b9cafac92e469
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-29 10:39:13 +0530

    some chnges

--- src/components/header.js
+++ src/components/header.js
@@ -88,65 +88,7 @@ export default function Header() {
             Home
           </Link>
 
-          {/* <Popover className="relative">
-            <Popover.Button
-              className={
-                "flex items-center text-sm font-semibold leading-6 text-gray-50 pb-2 hover:border-b-2 hover:border-blue-700 " +
-                (location.pathname === "/products"
-                  ? "border-b-2 border-blue-700"
-                  : "")
-              }
-            >
-              Product
-              <ChevronDownIcon
-                className="h-5 w-5 flex-none text-gray-400"
-                aria-hidden="true"
-              />
-            </Popover.Button>
-
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-400"
-              enterFrom="opacity-0 translate-y-20"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-gray-900 shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {products.map((item) => (
-                    <div
-                      key={item.id}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <img
-                          className="h-[20px] w-[20px] text-gray-600 group-hover:text-indigo-600"
-                          aria-hidden="true"
-                          src={item.image}
-                          alt={item.name}
-                        />
-                      </div>
-                      <div className="flex-auto">
-                        <Link
-                          to={"/products/" + item.id}
-                          className="block font-semibold text-gray-50 group-hover:text-black"
-                        >
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </Link>
-                        <p className="mt-1 text-gray-600">
-                          {item.description.split(".")[0]}
-                        </p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                
-              </Popover.Panel>
-            </Transition>
-          </Popover> */}
+          
 
           <Link
             to="/services"
@@ -205,85 +147,12 @@ export default function Header() {
       </nav>
 
 
-{/*       
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-950 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <Link to="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Peryton</span>
-              <img className="h-8 w-auto" src="imgs/logo.png" alt="" />
-            </Link>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <Link
-                  to="/"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-950"
-                >
-                  Home
-                </Link>
-
-                
-
-                <Link
-                  to="/services"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-950"
-                >
-                  Services
-                </Link>
-                <Link
-                  href="/about"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-50"
-                >
-                  About us
-                </Link>
-                <Link
-                  to="/ecommerce"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-50"
-                >
-                  Ecommerce
-                </Link>
-                <Link
-                  to="/contact"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-50"
-                >
-                  Contact
-                </Link>
-              </div>
-              <div className="py-6">
-                <Link
-                  to="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-50"
-                >
-                  <MagnifyingGlassIcon className="h-5 w-5 flex-none" />
-                </Link>
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog> */}
 
 
 {mobileMenuOpen && (
         <div className="fixed inset-0 z-10 overflow-y-auto bg-gray-950 px-6 py-6">
           <div className="flex items-center justify-between">
-            <Link to="#" className="-m-1.5 p-1.5">
+            <Link to="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Peryton</span>
               <img className="h-8 w-auto" src="imgs/logo.png" alt="" />
             </Link>
@@ -306,6 +175,16 @@ export default function Header() {
                 >
                   Home
                 </Link>
+
+             
+                <Link
+                  to="/ecommerce"
+                  onClick={()=>{setMobileMenuOpen(false)}}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-950"
+                >
+                  Products
+                </Link>
+
                 <Link
                   to="/services"
                   onClick={()=>{setMobileMenuOpen(false)}}
@@ -313,19 +192,13 @@ export default function Header() {
                 >
                   Services
                 </Link>
+               
                 <Link
                   to="/about"
                   onClick={()=>{setMobileMenuOpen(false)}}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-950"
                 >
                   About us
-                </Link>
-                <Link
-                  to="/ecommerce"
-                  onClick={()=>{setMobileMenuOpen(false)}}
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-950"
-                >
-                  Products
                 </Link>
                 <Link
                   to="/contact"
